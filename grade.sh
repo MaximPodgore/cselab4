7dcd9f76a4007b56e818750bdd31ac80bd131321
MaximPodgore <56902053+MaximPodgore@users.noreply.github.com>
2023-02-16 11:43:51 -0800

    Add files via upload

--- grade.sh
+++ grade.sh
@@ -1,4 +1,3 @@
-CPATH = ".;..lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
@@ -8,12 +7,22 @@ if [[ -e ListExamples.java ]]
     else 
         echo "We didn't find your file, next time don't screw up"
 fi
+cp -r ../lib ./
 cp ../TestListExamples.java ./
-javac -cp $CPATH *.java
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 echo 'We compiled!!'
 #if [[ $? -neq 0 ]]
     #then 
     #echo "Your code didn't compile, Paul Cao and Joe Politz would be disappointed"
 #fi   
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples >test.txt 
+cat test.txt
+head -2 test.txt | tail +2 > testCleaned.txt
+$Pass = grep -o -i . testCleaned.txt | wc -l 
+$Fail = grep -o -i E testCleaned.txt | wc -l 
+echo "You passed" 
+echo $Pass
+echo "tests out of" 
+echo $Pass + $Fail
+
 
